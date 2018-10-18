e535c9ce50f50d0290e3f84508287e20fcb10a6b
Josh Maximoff <jmaximoff@numurus.com>
2018-10-18 12:35:45 -0400

    Simplify trigger interface by setting trigger mask to fixed value
    This is a s/w-side simplification of the interface, removing the ability
    to adjust the mask value, fixing it instead to a bit offset.
    
    The num_sdk_zynq implementation provides the details.

--- src/trigger_interface.cpp
+++ src/trigger_interface.cpp
@@ -2,9 +2,10 @@
 
 namespace Numurus
 {
-TriggerInterface::TriggerInterface(SDKNode *parent):
+TriggerInterface::TriggerInterface(SDKNode *parent, uint32_t parent_trig_index):
 	trig_enabled{true},
-	trig_mask{"trig_mask", 0, parent},
+	trig_index{parent_trig_index},
+	//trig_mask{"trig_mask", 0, parent},
 	trig_delay{"trig_delay", 0, parent}
 {}
 
@@ -12,13 +13,8 @@ TriggerInterface::~TriggerInterface(){}
 
 void TriggerInterface::retrieveParams()
 {
-	trig_mask.retrieve();
+	//trig_mask.retrieve();
 	trig_delay.retrieve();
-}
-
-void TriggerInterface::setTrigMask(const std_msgs::UInt32::ConstPtr& trig_mask_val)
-{
-	trig_mask = trig_mask_val->data;
 }
 
 void TriggerInterface::setTrigDelay(const std_msgs::UInt32::ConstPtr& trig_delay_val_usecs)
