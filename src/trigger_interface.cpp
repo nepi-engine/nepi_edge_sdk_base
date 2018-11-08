1e101d548b15840a57ab79475cfe384e334c769d
Josh Maximoff <jmaximoff@numurus.com>
2018-11-08 11:03:42 -0500

    Use device namespace for the trigger_index_settings rather than private node namespace
    This fixes a bug that kept TriggerMgr from receiving these messages properly (as it didn't subscribe t the topic under node-specific namespaces). Now all nodes w/ a TriggerInterface publish on the same universal topic.

--- src/trigger_interface.cpp
+++ src/trigger_interface.cpp
@@ -27,7 +27,7 @@ void TriggerInterface::initPublishers()
 	SDKInterface::initPublishers();
 
 	// Advertise and publish (with latch) the trigger index so trig mgr. can populate trig defs appropriately
-	_trig_index_pub = _parent_priv_nh->advertise<num_sdk_msgs::TriggerIndexSettings>("trigger_index_settings", 5, true); // true to latch it; ensure it is sent on each new subscriber connection
+	_trig_index_pub = _parent_pub_nh->advertise<num_sdk_msgs::TriggerIndexSettings>("trigger_index_settings", 5, true); // true to latch it; ensure it is sent on each new subscriber connection
 	
 	// Publish TriggerIndexSettings once to latch automatic response to future subscribers
 	num_sdk_msgs::TriggerIndexSettings index_settings;
