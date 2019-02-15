981ce4b5535718b466aa351a602abd7225267c16
Alex McMaster <alex.r.mcmaster@gmail.com>
2019-02-14 16:00:08 -0800

    nepi_core_mgr: add process_updates service
    This service goes through the appropriate directories within the
    NEPI-Bot filesystem interface and processing configuration update
    files, passing their contents to lpp_mgr and on to the LPP for
    execution.

--- scripts/nepi_core_mgr.py
+++ scripts/nepi_core_mgr.py
@@ -5,9 +5,12 @@ import rospy
 import json
 import os
 import datetime
+import re
 
 from num_sdk_msgs.msg import OotStatus
-from num_sdk_base.srv import NepiDataDir, NepiDataDirResponse
+from num_sdk_base.srv import NepiDataDir, NepiDataDirResponse, ProcessCfgUpdates, ProcessCfgUpdatesResponse
+from num_sdk_sb2.srv import LppConfigActionSequence, LppConfigGeofence, LppConfigSmartTriggerRule,\
+        LppConfigTaskSchedule, LppConfigSensor, LppConfigSmartTrigger
 
 NEPI_ROOT = "/home/nepi-usr"
 
@@ -87,6 +90,213 @@ class NepiCoreMgr:
     def get_current_data_dir(self, req):
         return NepiDataDirResponse(self.current_data_dir)
 
+    def _call_service(self, srv_name, srv_type, srv_args):
+        try:
+            rospy.wait_for_service(srv_name)
+        except rospy.RosException, exc:
+            rospy.logwarn("{} failed: {}".format(exc))
+            return None
+        try:
+            proxy = rospy.ServiceProxy(srv_name, srv_type)
+            resp = proxy(*srv_args)
+        except rospy.ServiceException, exc:
+            return None
+
+        return resp
+
+    def _process_one_action_update(self, fn):
+        if not re.match("^action_seq_cfg_\\d+\\.json$", fn):
+            rospy.logerr("Invalid name for action cfg file: {}".format(fn))
+            return
+
+        tmp = re.search("\\d+", fn)
+        version_index = int(fn[tmp.start():tmp.end()])
+
+        fullpath = os.path.join(NEPI_ROOT, "action", fn)
+        with open(fullpath) as f:
+            data = json.load(f)
+
+        action_seq_index = data["action_seq_id"]
+        actions = [item["action_id"] for item in data["actions"]]
+        max_durations = [item["max_duration"] for item in data["actions"]]
+
+        resp = self._call_service("lpp_config_action_sequence", LppConfigActionSequence,
+                (version_index, action_seq_index, actions, max_durations))
+
+        if resp is not None and resp.success:
+            rospy.loginfo("Successfully processed {}".format(fn))
+        else:
+            rospy.logerr("Failed to process {}".format(fn))
+
+    def process_action_updates(self):
+        action_updates = os.listdir(os.path.join(NEPI_ROOT, "action"))
+        for fn in action_updates:
+            self._process_one_action_update(fn)
+
+    def _process_one_geofence_update(self, fn):
+        if not re.match("^geofence_cfg_\\d+\\.json$", fn):
+            rospy.logerr("Invalid name for geofence cfg file: {}".format(fn))
+            return
+
+        tmp = re.search("\\d+", fn)
+        version_index = int(fn[tmp.start():tmp.end()])
+
+        fullpath = os.path.join(NEPI_ROOT, "geofence", fn)
+        with open(fullpath) as f:
+            data = json.load(f)
+
+        polygon_id = data["polygon_id"]
+        enabled = data["enabled"]
+        lat_vertices = [item["lat"] for item in data["vertices"]]
+        lon_vertices = [item["long"] for item in data["vertices"]]
+
+        resp = self._call_service("lpp_config_geofence", LppConfigGeofence,
+                (version_index, polygon_id, enabled, lat_vertices, lon_vertices))
+
+        if resp is not None and resp.success:
+            rospy.loginfo("Successfully processed {}".format(fn))
+        else:
+            rospy.logerr("Failed to process {}".format(fn))
+
+    def process_geofence_updates(self):
+        geofence_updates = os.listdir(os.path.join(NEPI_ROOT, "geofence"))
+        for fn in geofence_updates:
+            self._process_one_geofence_update(fn)
+
+    def _process_one_rules_update(self, fn):
+        if not re.match("^smarttrig_rule_\\d+\\.json$", fn):
+            rospy.logerr("Invalid name for rules cfg file: {}".format(fn))
+            return
+
+        tmp = re.search("\\d+", fn)
+        version_index = int(fn[tmp.start():tmp.end()])
+
+        fullpath = os.path.join(NEPI_ROOT, "rules", fn)
+        with open(fullpath) as f:
+            data = json.load(f)
+
+        line = data["rule_id"]
+        enabled = data["enabled"]
+        smarttrig_ids = [item["smarttrig_id"] for item in data["premises"]]
+        # FIXME: deal with this "instance" thing.
+        smarttrig_freq_threshes = [item["freq_thresh"] for item in data["premises"]]
+        action = data["action_seq_id"]
+        min_period = data["min_period"]
+
+        resp = self._call_service("lpp_config_smart_trigger_rule", LppConfigSmartTriggerRule,
+                (version_index, line, enabled, smarttrig_ids, smarttrig_freq_threshes, action, min_period))
+
+        if resp is not None and resp.success:
+            rospy.loginfo("Successfully processed {}".format(fn))
+        else:
+            rospy.logerr("Failed to process {}".format(fn))
+
+    def process_rules_updates(self):
+        rules_updates = os.listdir(os.path.join(NEPI_ROOT, "rules"))
+        for fn in rules_updates:
+            self._process_one_rules_update(fn)
+
+    def _process_one_sched_update(self, fn):
+        if not re.match("^task_cfg_\\d+\\.json$", fn):
+            rospy.logerr("Invalid name for task cfg file: {}".format(fn))
+            return
+
+        tmp = re.search("\\d+", fn)
+        version_index = int(fn[tmp.start():tmp.end()])
+
+        fullpath = os.path.join(NEPI_ROOT, "sched", fn)
+        with open(fullpath) as f:
+            data = json.load(f)
+
+        line = data["line"]
+        start_time = data["start_time"]
+        period = data["period"]
+        max_repetition = data["max_repetition"]
+        action_seq_index = data["action_seq_id"]
+
+        resp = self._call_service("lpp_config_task_schedule", LppConfigTaskSchedule,
+                (version_index, line, start_time, period, max_repetition, action_seq_index))
+
+        if resp is not None and resp.success:
+            rospy.loginfo("Successfully processed {}".format(fn))
+        else:
+            rospy.logerr("Failed to process {}".format(fn))
+
+    def process_sched_updates(self):
+        sched_updates = os.listdir(os.path.join(NEPI_ROOT, "sched"))
+        for fn in sched_updates:
+            self._process_one_sched_update(fn)
+
+    def _process_one_sensors_update(self, fn):
+        if not re.match("^sensor_cfg_\\d+\\.json$", fn):
+            rospy.logerr("Invalid name for sensor cfg file: {}".format(fn))
+            return
+
+        tmp = re.search("\\d+", fn)
+        version_index = int(fn[tmp.start():tmp.end()])
+
+        fullpath = os.path.join(NEPI_ROOT, "sensors", fn)
+        with open(fullpath) as f:
+            data = json.load(f)
+
+        sensor_id = data["sensor_id"]
+        config_params = [item["param_id"] for item in data["params"]]
+        values = [item["value"] for item in data["params"]]
+
+        resp = self._call_service("lpp_config_sensor", LppConfigSensor,
+                (version_index, sensor_id, config_params, values))
+
+        if resp is not None and resp.success:
+            rospy.loginfo("Successfully processed {}".format(fn))
+        else:
+            rospy.logerr("Failed to process {}".format(fn))
+
+    def process_sensors_updates(self):
+        sensors_updates = os.listdir(os.path.join(NEPI_ROOT, "sensors"))
+        for fn in sensors_updates:
+            self._process_one_sensors_update(fn)
+
+    def _process_one_trig_update(self, fn):
+        if not re.match("^smarttrig_cfg_\\d+\\.json$", fn):
+            rospy.logerr("Invalid name for smarttrig cfg file: {}".format(fn))
+            return
+
+        tmp = re.search("\\d+", fn)
+        version_index = int(fn[tmp.start():tmp.end()])
+
+        fullpath = os.path.join(NEPI_ROOT, "trig", fn)
+        with open(fullpath) as f:
+            data = json.load(f)
+
+        smarttrig_id = data["smarttrig_type"]
+        smarttrig_type_index = data["instance"]
+        config_params = [item["param_id"] for item in data["params"]]
+        values = [item["value"] for item in data["params"]]
+
+        resp = self._call_service("lpp_config_smart_trigger", LppConfigSmartTrigger,
+                (version_index, smarttrig_id, smarttrig_type_index, config_params, values))
+
+        if resp is not None and resp.success:
+            rospy.loginfo("Successfully processed {}".format(fn))
+        else:
+            rospy.logerr("Failed to process {}".format(fn))
+
+    def process_trig_updates(self):
+        trig_updates = os.listdir(os.path.join(NEPI_ROOT, "trig"))
+        for fn in trig_updates:
+            self._process_one_trig_update(fn)
+
+    def process_cfg_updates(self, req):
+        # TODO: for each: archive processed files
+        # TODO: for each: file locking
+        self.process_action_updates()
+        self.process_geofence_updates()
+        self.process_rules_updates()
+        self.process_sched_updates()
+        self.process_sensors_updates()
+        self.process_trig_updates()
+        return ProcessCfgUpdatesResponse()
+
     def run(self):
         rospy.spin()
 
@@ -99,6 +309,7 @@ class NepiCoreMgr:
 
         self.current_data_dir = ""
         rospy.Service("get_current_data_dir", NepiDataDir, self.get_current_data_dir)
+        rospy.Service("process_cfg_updates", ProcessCfgUpdates, self.process_cfg_updates)
 
         self.run()
 
