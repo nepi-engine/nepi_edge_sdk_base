5021c17842eab337a2c56e00046900375011c2f6
Josh Maximoff <jmaximoff@numurus.com>
2021-05-04 14:17:15 -0400

    Add new h/w-level drivers
    This supports sonar development, but the drivers are generic so adding them to num_sdk_base.
    
    i2c: Linux i2c API wrapper functions
    ltc1669_dac: DAC used in illumination boards and CBX board for variable TX power.

---
+++ include/drivers/i2c.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+typedef struct
+{
+  int fd = 0;
+} i2c_struct_t;
+
+int i2c_init( i2c_struct_t *i2c, uint32_t bus_id );
+int i2c_write( const i2c_struct_t* const i2c, uint8_t addr, uint8_t *data, int len );
+int i2c_read( const i2c_struct_t* const i2c, uint8_t addr, uint8_t *data, int len );
---
+++ include/drivers/ltc1669_dac.h
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+typedef struct
+{
+  i2c_struct_t i2c;
+  uint8_t addr_7_bit = 0x0;
+  uint16_t curr_dac_val = 0x0;
+  uint8_t initialized = 0;
+} ltc1669_dac_struct_t;
+
+int ltc1669_dac_init(ltc1669_dac_struct_t *dac, uint32_t i2c_bus_id, uint8_t addr_7_bit);
+int ltc1669_dac_write(ltc1669_dac_struct_t* dac, uint16_t val);
+inline uint16_t ltc1669_dac_get_current_val(const ltc1669_dac_struct_t* const dac){return dac->curr_dac_val;}
