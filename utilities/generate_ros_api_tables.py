01844d52a35f76a974cb59316adb9c756e7332b8
Josh Maximoff <jmaximoff@numurus.com>
2023-12-13 14:22:17 -0500

    Add license headers and LICENSE file
    Exclude create_ap and wondershaper directories because they are separately-licensed 3rd-party tools

--- utilities/generate_ros_api_tables.py
+++ utilities/generate_ros_api_tables.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+#
+# NEPI Dual-Use License
+# Project: nepi_edge_sdk_base
+#
+# This license applies to any user of NEPI Engine software
+#
+# Copyright (C) 2023 Numurus, LLC <https://www.numurus.com>
+# see https://github.com/numurus-nepi/nepi_edge_sdk_base
+#
+# This software is dual-licensed under the terms of either a NEPI software developer license
+# or a NEPI software commercial license.
+#
+# The terms of both the NEPI software developer and commercial licenses
+# can be found at: www.numurus.com/licensing-nepi-engine
+#
+# Redistributions in source code must retain this top-level comment block.
+# Plagiarizing this software to sidestep the license obligations is illegal.
+#
+# Contact Information:
+# ====================
+# - https://www.numurus.com/licensing-nepi-engine
+# - mailto:nepi@numurus.com
+#
+#
+
 
 # This script is intended to be run from a system with the proper ROS setup.bash file sourced so that all message types are understood.
 # Generally, run it from the NEPI device itself
